7d9152102bca053f7fc96787910e1c38b5e07938
Abhiram13 <40509745+Abhiram13@users.noreply.github.com>
2020-02-24 21:47:12 +0530

    Linked Bootstrap and established connection betwen node and react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameValue: '',
+      secondNameValue: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  async callBackendAPI() {
+    const response = await fetch('/get');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }  
+
+  handleChange = event => {
+    if (event.target.id === 'name') {
+      this.setState({ nameValue: event.target.value })
+    } else {
+      this.setState({ secondNameValue: event.target.value })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container pt-5">
+          <div className="d-flex col-sm-5 p-0 justify-content-between">
+            <input type="text" id="name" value={this.state.nameValue} onChange={this.handleChange} />
+            <input type="text" id="secondName" value={this.state.secondNameValue} onChange={this.handleChange} />
+            <button type="button" className="btn btn-info">Submit</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
