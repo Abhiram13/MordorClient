27e84e8c80f3e4daa945edf8e299c4599381b7dc
Abhiram13 <40509745+Abhiram13@users.noreply.github.com>
2020-02-27 15:50:44 +0530

    Added workflow folders and installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 /* eslint-disable no-lone-blocks */
 import React from 'react';
-import postRequest from '../src/helpers/helper';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Auth from './workflow/Authenticate/auth'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nameValue: '',
-      secondNameValue: '',
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  /**
-   * @param {object} event - returns the element by id which triggered the Event
-   */
-  handleChange = event => {
-    if (event.target.id === 'name') {
-      this.setState({ nameValue: event.target.value })
-    } else {
-      this.setState({ secondNameValue: event.target.value })
-    }
-  }
-
-  sendDate() {
-    postRequest('post', '/app.js', this.state);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="container pt-5">
-          <div className="d-flex col-sm-5 p-0 justify-content-between">
-            <input type="text" id="name" value={this.state.nameValue} onChange={this.handleChange} />
-            <input type="text" id="secondName" value={this.state.secondNameValue} onChange={this.handleChange} />
-            <button type="button" className="btn btn-info" onClick={() => this.sendDate()}>Submit</button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Router>      
+      <Route exact path="/" 
+      // @ts-ignore
+      component={Auth} />
+    </Router>
+  )
 }
 
 export default App;
