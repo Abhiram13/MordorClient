70f45ebce446804d81a95915be886e63926a7cc9
Abhiram13 <40509745+Abhiram13@users.noreply.github.com>
2020-03-14 12:18:58 +0530

    Converted Signup Component to Functional Component

--- src/workflow/Authenticate/signin.js
+++ src/workflow/Authenticate/signin.js
@@ -1,101 +1,70 @@
 /* eslint-disable no-lone-blocks */
-import React from 'react';
+import React, { useState } from 'react';
 
-class SignUp extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         username: '',
-         firstname: '',
-         lastname: '',
-         password: '',
-      }
-      this.handleChange = this.handleChange.bind(this);
-   }
-
-   /**
-    * @param {object} event - returns the element by id which triggered the Event
-    */
-   handleChange = event => {
-      switch (event.target.id) {
-         case 'username':
-            this.setState({
-               username: event.target.value,
-            });
-            break;
-         case 'firstname':
-            this.setState({
-               firstname: event.target.value,
-            });
-            break;
-         case 'lastname':
-            this.setState({
-               lastname: event.target.value,
-            });
-            break;
-         case 'password':
-            this.setState({
-               password: event.target.value,
-            });
-            break;
-         default:
-            return null;
-      }
-   }
-
-   sendDate() {
-      const { username, firstname, lastname, password } = this.state;
-      
-      if (username && firstname && lastname && password) {
-         this.props.create(this.state);
-      }
-   }
-
-   render() {
-      return (
-         <div className="container pt-5">
-            <div className="col-sm-6 p-0 mx-auto rounded shadow">
-               <div className="p-3">
-                  <h4 className="pt-2">Sign Up</h4>
-                  <hr className="" />
-                  <input
-                     type="text"
-                     id="username"
-                     placeholder="User Name"
-                     className="d-block col-sm p-0 rounded border p-2 mb-3"
-                     value={this.state.username}
-                     onChange={this.handleChange} />
-                  <input
-                     type="text"
-                     id="firstname"
-                     placeholder="First Name"
-                     className="d-block col-sm p-0 rounded border p-2 mb-3"
-                     value={this.state.firstname}
-                     onChange={this.handleChange} />
-                  <input
-                     type="text"
-                     id="lastname"
-                     placeholder="Last Name"
-                     className="d-block col-sm p-0 rounded border p-2 mb-3"
-                     value={this.state.lastname}
-                     onChange={this.handleChange} />
-                  <input
-                     type="text"
-                     id="password"
-                     placeholder="Password"
-                     className="d-block col-sm p-0 rounded border p-2 mb-3"
-                     value={this.state.password}
-                     onChange={this.handleChange} />
-                  <button
-                     type="button"
-                     className="btn btn-info d-block mx-auto col-sm-3 py-1"
-                     onClick={() => this.sendDate()}>Submit</button>
-               </div>
-               <p className="text-info text-center m-0 p-0 pointer"><small onClick={() => this.props.exist(true)}>Existing User?</small></p>
-            </div>
-         </div>
-      );
-   }
+function handleChange(event, stateFunction) {
+   stateFunction(event.target.value);
 }
 
-export default SignUp;
+function sendDate(userName, firstName, lastName, passWord, props) {
+   if (userName && firstName && lastName && passWord) {
+      props.create({
+         userName: userName,
+         firstName: firstName,
+         lastName: lastName,
+         passWord: passWord,
+      })
+   }
+
+   return;
+}
+
+export default function SignUp(props) {
+   const [userName, setUserName] = useState('');
+   const [firstName, setFirstName] = useState('');
+   const [lastName, setLastName] = useState('');
+   const [passWord, setPassword] = useState('');
+
+   return (
+      <div className="container pt-5">
+         <div className="col-sm-6 p-0 mx-auto rounded shadow">
+            <div className="p-3">
+               <h4 className="pt-2">Sign Up</h4>
+               <hr className="" />
+               <input
+                  type="text"
+                  id="username"
+                  placeholder="User Name"
+                  className="d-block col-sm p-0 rounded border p-2 mb-3"
+                  value={userName}
+                  onChange={(event) => handleChange(event, setUserName)} />
+               <input
+                  type="text"
+                  id="firstname"
+                  placeholder="First Name"
+                  className="d-block col-sm p-0 rounded border p-2 mb-3"
+                  value={firstName}
+                  onChange={(event) => handleChange(event, setFirstName)} />
+               <input
+                  type="text"
+                  id="lastname"
+                  placeholder="Last Name"
+                  className="d-block col-sm p-0 rounded border p-2 mb-3"
+                  value={lastName}
+                  onChange={(event) => handleChange(event, setLastName)} />
+               <input
+                  type="text"
+                  id="password"
+                  placeholder="Password"
+                  className="d-block col-sm p-0 rounded border p-2 mb-3"
+                  value={passWord}
+                  onChange={(event) => handleChange(event, setPassword)} />
+               <button
+                  type="button"
+                  className="btn btn-info d-block mx-auto col-sm-3 py-1"
+                  onClick={() => sendDate(userName, firstName, lastName, passWord, props)}>Submit</button>
+            </div>
+            <p className="text-info text-center m-0 p-0 pointer"><small onClick={() => props.exist(true)}>Existing User?</small></p>
+         </div>
+      </div>
+   );
+}
