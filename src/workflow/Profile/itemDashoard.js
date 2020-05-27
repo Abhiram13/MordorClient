8ef07497a62a4ad3791f21b330ced97a51b53be2
Abhiram Nagadi <abhiramnagadi@Abhirams-MacBook-Air.local>
2020-05-27 10:54:08 +0530

    item list has been added in items dashboard

--- src/workflow/Profile/itemDashoard.js
+++ src/workflow/Profile/itemDashoard.js
@@ -1,10 +1,51 @@
 import React, { Fragment } from 'react';
+import request from '../../helpers/helper';
 
 export default class ItemDashboard extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         items: '',
+      }
+   }
+
+   componentDidMount() {
+      request.get('getItem').then((data) => {
+         this.setState({
+            items: data.documents,
+         })
+      })
+   }
+
    render() {
+      console.log(this.state.items);
       return (
          <Fragment>
-            <h4>This is Item Dashboard</h4>
+            {
+               (this.state.items) &&
+               <table className="table table-striped">
+                  <thead>
+                     <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Item Name</th>
+                        <th scope="col">Item Category</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {
+                        this.state.items.map((item, index) => {
+                           return (
+                              <tr key={item.itemName}>
+                                 <th scope="row">{index + 1}</th>
+                                 <td>{item.itemName}</td>
+                                 <td>{item.category}</td>
+                              </tr>
+                           )
+                        })
+                     }
+                  </tbody>
+               </table>
+            }
          </Fragment>
       )
    }
